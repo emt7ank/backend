0f1514e68e3d18d75ea76d803b3e3aede40762eb
MohamedElshafeay <mohamedelshafeay@gmail.com>
2019-07-04 03:47:03 +0200

    editing config.ini

--- emt7ank/settings.py
+++ emt7ank/settings.py
@@ -146,8 +146,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-CORS_ORIGIN_ALLOW_ALL = True
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 INTERNAL_IPS = ['127.0.0.1',]
+CORS_ORIGIN_ALLOW_ALL = True
 
 from configparser import RawConfigParser
 
